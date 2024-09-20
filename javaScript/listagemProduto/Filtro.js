0f92639988168cb5977ea920221c145bb37c7e2d
jcavalcantee <jcavalcantessilva@gmail.com>
2024-09-20 20:29:39 -0300

    adicionado filtro nas paginacoes de produto

--- javaScript/listagemProduto/Filtro.js
+++ javaScript/listagemProduto/Filtro.js
@@ -1,15 +1,46 @@
-function filtrarProdutos() {
-    var filtro = document.getElementById('filtro').value.toUpperCase();
- 
-    var produtos = document.querySelectorAll("tbody tr");
-  
-    produtos.forEach(produtos => {
-        var nomeProduto = produtos.querySelector("td:nth-child(2)").innerText.toUpperCase();
+// var API = "4.228.231.149"; //Setar essa variavel quando subir para a nuvem e comentar a localhost
+var API = "localhost"; //Setar essa variavel quando testar local e comentar a do IP
 
-        if (nomeProduto.includes(filtro)) {
-            produtos.style.display = ""; 
-        } else {
-            produtos.style.display = "none";
+var token = localStorage.getItem("tokenAcesso");
+
+function filtrarProdutos() {
+    // Obtém o valor digitado no campo de entrada e remove espaços extras
+    var filtro = document.getElementById('filtro').value.trim().toUpperCase();
+
+    // Se o filtro estiver vazio, busca os produtos normalmente
+    if (!filtro) {
+        fetchProdutoData(); // Carrega a lista completa se o filtro estiver vazio
+        return;
+    }
+
+    async function fetchFilteredData(page = 0) {
+        try {
+            const response = await fetch(`http://${API}:8080/api/produtos/buscarNome?nome=${filtro}&page=${page}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            
+            // Se a API retornar 404, exibe a mensagem de produto não encontrado
+            if (response.status === 404) {
+                alert('erro de conexão com a API')
+                return;
+            }
+
+            const result = await response.json();
+            if (result.produtos.length === 0) {
+                // Caso a lista de produtos retornada seja vazia, exibe mensagem de produto não encontrado
+                alert("Produto não encontrado")
+            } else {
+                data = result.produtos; // Armazena os produtos filtrados
+                totalPages = result.totalPages; // Armazena o número total de páginas para os produtos filtrados
+                displayTableData(); // Exibe os dados filtrados
+                setupPagination(); // Configura a paginação para os dados filtrados
+            }
+        } catch (error) {
+            console.error('Erro ao buscar dados filtrados:', error);
         }
-    });
+    }
+
+    fetchFilteredData(); // Chama a função para buscar os produtos filtrados
 }
