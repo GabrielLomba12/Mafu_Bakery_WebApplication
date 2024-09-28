63c08ae1b5bc17e2eab1cb74f6a37f0a527f2d06
jcavalcantee <jcavalcantessilva@gmail.com>
2024-09-28 15:47:39 -0300

    tela inicial com a listagem dos produtos

---
+++ javaScript/telaInicial/MontarLayoutProdutos.js
@@ -0,0 +1,54 @@
+// var API = "4.228.231.149"; //Setar essa variavel quando subir para a nuvem e comentar a localhost
+var API = "localhost"; //Setar essa variavel quando testar local e comentar a do IP
+
+data = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchProduto();
+})
+
+async function fetchProduto() {
+    try {
+        const response = await fetch(`http://${API}:8080/api/produtos/exibirTodos`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const result = await response.json();
+        console.log(result);
+        data = result;
+        montarLayoutExibicao(data);
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+    }
+}
+
+function montarLayoutExibicao(produtos) {
+    const listaProdutos = document.getElementById('product-list');
+    listaProdutos.innerHTML = '';
+
+
+    let produtosHTML = '';
+
+    produtos.forEach(produto => {
+        if (produto.status) {
+            produtosHTML += `
+                <div class="col product-col">
+                    <div class="card h-100">
+                        <img src="${produto.imagens[0]}" class="card-img-top" alt="${produto.nome}">
+                        <div class="card-body">
+                            <h5 class="card-title">${produto.nome}</h5>
+                            <p class="card-text"><strong>R$ ${formatarCasasDecimais(produto.preco)}</strong></p>
+                            <p>Avaliação: ${produto.avaliacao}</p>
+                            <a href="#" class="btn btn-primary">Detalhes do produto</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    });
+
+    listaProdutos.innerHTML = produtosHTML;
+}
+
---
+++ javaScript/telaInicial/Utils.js
@@ -0,0 +1,7 @@
+function formatarCasasDecimais(numero) {
+    return Number(numero).toFixed(2);
+}
+
+function redirecionarTelaLogin() {
+    window.location.href = "Login.html"
+}
