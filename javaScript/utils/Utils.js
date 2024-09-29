5ab386d21c4053d6db6368f0286306c5ad391017
GabrielLomba12 <lombagabriel324@gmail.com>
2024-09-29 15:06:51 -0300

    feature: tela detalhe de produto

--- javaScript/utils/Utils.js
+++ javaScript/utils/Utils.js
@@ -1,3 +1,7 @@
 function redirecionarTelaInicial() {
     window.location.href = "TelaInicial.html"
 }
+
+function redirecionarDetalheProduto(){
+    window.location.href = "TelaDetalheProduto.html"
+}
