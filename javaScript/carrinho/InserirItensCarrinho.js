07a354f75c5e307172bc3f2efcb84fdfadc3b005
GabrielLomba12 <lombagabriel324@gmail.com>
2024-10-04 20:55:21 -0300

    esilização do carrinho

--- javaScript/carrinho/InserirItensCarrinho.js
+++ javaScript/carrinho/InserirItensCarrinho.js
@@ -1,42 +1,18 @@
+const produtosCabecalho = document.querySelector('.box-titulos')
+
 function exibirProdutosCarrinho() {
     const produtosCarrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-    const produtosContainer = document.querySelector('.produtos-carrinho');
     const produtosMap = {};
-    produtosContainer.innerHTML = ''; 
-
     let valorTotal = 0;
 
     produtosCarrinho.forEach(produto => {
         produtosMap[produto.id] = {...produto};
     });
 
-    const produtosCabecalho = document.createElement('div');
-    produtosCabecalho.classList.add('produto');
-    produtosCabecalho.innerHTML = 
-    `
-    <div class="box-titulos">
-        <div class="title-imagem">
-            <p id="t">Produto</p>
-        </div>
-        <div class="title-nome">
-            <p id="t">Nome</p>
-        </div>
-        <div class="title-qtd">
-            <p id="t">Quantidade</p>
-        </div>
-        <div class="title-valor">
-            <p id="t">Valor Unitário</p>
-        </div>
-        <div class="title-total">
-            <p id="t">Valor Total</p>   
-        </div>
-    </div>
-    `;
-    produtosContainer.appendChild(produtosCabecalho);
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
     for (const id in produtosMap) {
         const produto = produtosMap[id];
-        const produtoElement = document.createElement('div');
-        produtoElement.classList.add('produto');
 
         const valorUnitario = parseFloat(produto.preco);
         const valorTotalProduto = valorUnitario * produto.quantidade;
@@ -47,43 +23,27 @@ function exibirProdutosCarrinho() {
         imgElement.alt = `${produto.nome} imagem`;
         imgElement.style.width = "100px"; 
 
+        
+        const produtoElement = document.createElement('tr');
         produtoElement.innerHTML = 
         `
-            <div class="box-titulos">
-                <div class="title-imagem">
-                    <div class="info-produto">
-                        ${imgElement.outerHTML} <!-- Adiciona a imagem -->
-                    </div>
-                </div>
-                <div class="title-nome">
-                    <div class="info-nome">
-                        <p>${produto.nome}</p>
-                    </div>
-                </div>
-                <div class="title-qtd">
-                    <div class="info-quantidade">
-                        <button onclick="diminuirQuantidade(${produto.id})">-</button>
-                        <p>${produto.quantidade}</p>
-                        <button onclick="aumentarQuantidade(${produto.id})">+</button>
-                    </div>
-                </div>
-                <div class="title-valor">
-                    <div class="info-valor">
-                        <p>R$ ${valorUnitario.toFixed(2)}</p>
-                    </div>
-                </div>
-                <div class="title-total">
-                    <div class="info-total">
-                        <p>R$ ${valorTotalProduto.toFixed(2)}</p>
-                    </div>   
-                </div>
-                <div class="btn-lixo">
-                    <button onclick="excluirProduto(${produto.id})">Lixo</button>
-                </div>
-            </div> 
-        `;
+            <td>${imgElement.outerHTML}</td>
+            <td>${produto.nome}</td>
+            <td class = "qtd">
+                <button onclick="diminuirQuantidade(${produto.id})">-</button>
+                <p>${produto.quantidade}</p>
+                <button onclick="aumentarQuantidade(${produto.id})">+</button>
+            </td>
+            <td>R$ ${valorUnitario.toFixed(2)}</td>
+            <td>R$ ${valorTotalProduto.toFixed(2)}</td>
 
-        produtosContainer.appendChild(produtoElement);
+            <td>
+                <button id="excluir" onclick="excluirProduto(${produto.id})">
+                    <i class="fas fa-trash-alt"></i>
+                </button>
+            </td>
+        `;
+        tbody.appendChild(produtoElement)
     }
 
     document.getElementById('valor-produtos').innerText = `R$ ${valorTotal.toFixed(2)}`;
